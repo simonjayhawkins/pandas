4685159e792321c82a507923c580d1ef5a86f264
Simon Hawkins <simonjayhawkins@gmail.com>
2020-09-06 15:05:04 +0100

    wip

---
+++ bisect/31954.py
@@ -0,0 +1,20 @@
+import pandas
+import numpy as np
+
+a = np.array([[1, 2], [3, 4]])
+
+# DO NOT WORKS
+b = np.array([[0.5, 6], [7, 8]])
+# b = np.array([[.5,6],[7,8]])  # The same problem
+
+# This one works fine:
+# b = np.array([[5,6],[7,8]])
+
+dfA = pandas.DataFrame(a)
+# This works fine EVEN using .5, because the columns name is different
+# dfA = pandas.DataFrame(a, columns=['a','b'])
+dfB = pandas.DataFrame(b)
+
+df_new = pandas.concat([dfA, dfB], axis=1)
+
+print(df_new[df_new > 5])
