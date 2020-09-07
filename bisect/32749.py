2521f051f4129d616caacbe9fe74dea77ae716b4
Simon Hawkins <simonjayhawkins@gmail.com>
2020-09-07 15:13:26 +0100

    add code sample for #32749

---
+++ bisect/32749.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+from pandas.tests.extension.decimal import DecimalArray, make_data
+
+s = pd.Series(DecimalArray(make_data()[:5]))
+
+print(s)
+
+try:
+    s.idxmin()
+except TypeError as err:
+    msg = "reduction operation 'argmin' not allowed for this dtype"
+    assert str(err) == msg
