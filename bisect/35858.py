3fc4ca6eaeacb8e7a46be7b38e24912ff352e0f5
Simon Hawkins <simonjayhawkins@gmail.com>
2020-09-07 14:25:39 +0100

    add code sample for #35858

---
+++ bisect/35858.py
@@ -0,0 +1,15 @@
+import pandas
+import pandas.testing as tm
+
+print(pandas.__version__)
+
+date = pandas.Timestamp("2000")
+x = pandas.DataFrame([["a", date, 1],], columns=["a", "b", "c"]).set_index(["a", "b"])[
+    "c"
+]
+print(x)
+
+res = x.loc[:, [date]]
+print(res)
+
+tm.assert_series_equal(res, x)
