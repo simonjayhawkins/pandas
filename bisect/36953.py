4d61410f0155ff44cd4c3b877403f536df78d05a
Simon Hawkins <simonjayhawkins@gmail.com>
2020-10-09 14:44:20 +0100

    code sample for #36953

---
+++ bisect/36953.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+print(pd.__version__)
+
+increment = pd.Series(pd.to_timedelta(30 + np.random.randn(1000), unit="s")).cumsum()
+increment[-1] = None
+index = pd.Timestamp("2012-01-01 09:00") + increment
+df = pd.Series(range(len(index)), index=index).to_frame()
+print(df)
+
+res = df["2012-01-01":"2012-01-05"]
+print(res)
