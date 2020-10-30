69073c6ca52cee13dafacce6cdf2ceb048817e6c
Simon Hawkins <simonjayhawkins@gmail.com>
2020-10-30 11:41:18 +0000

    add code sample for #37465

---
+++ bisect/37465.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+print(pd.__version__)
+
+g = np.random.default_rng(840812492384587325982704)
+a = pd.Series(g.integers(0, 3, size=100)).astype("category")
+b = pd.Series(g.integers(0, 2, size=100)).astype("category")
+res = pd.crosstab(a, b, margins=True, dropna=False)
+print(res)
