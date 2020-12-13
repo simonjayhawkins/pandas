4c576b09a15e60494cd6ef8a7be864b618b1907e
Simon Hawkins <simonjayhawkins@gmail.com>
2020-12-13 16:36:27 +0000

    add code sample for #38434

---
+++ bisect/38434.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+print(pd.__version__)
+
+ci = pd.CategoricalIndex(["a", "b"])
+df = pd.DataFrame([[1, 2], [3, 4]], index=ci, columns=ci)
+
+result = df.shift(axis=1)
+print(result)
+
+expected = pd.DataFrame([[np.nan, 1.0], [np.nan, 3.0]], index=ci, columns=ci)
+
+import pandas.testing as tm
+
+tm.assert_frame_equal(result, expected)
