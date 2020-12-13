422f3cf1116541fd1466d00d0d2da99f83970fdc
Simon Hawkins <simonjayhawkins@gmail.com>
2020-12-13 12:15:37 +0000

    code sample for #38419

---
+++ bisect/38419.py
@@ -0,0 +1,32 @@
+# BUG: set_index screws up the dtypes on empty DataFrames #38419
+
+import pandas as pd
+
+print(pd.__version__)
+
+d1 = pd.DataFrame(
+    {
+        "a": pd.Series(dtype="datetime64[ns]"),
+        "b": pd.Series(dtype="int64"),
+        "c": [],
+    }
+)
+# d1
+
+# d1.dtypes
+
+d2 = d1.set_index(["a", "b"])
+# d2
+
+# d2.dtypes
+
+result = d2.index.to_frame().dtypes
+print(result)
+
+expected = d1.loc[:, ["a", "b"]].dtypes
+print(expected)
+
+# assert (d1.loc[:, ["a", "b"]].dtypes == d2.index.to_frame().dtypes).all()
+import pandas.testing as tm
+
+tm.assert_series_equal(result, expected)
