c32b80bd10a4bc10e5920abb3e4616af268dfcf9
Simon Hawkins <simonjayhawkins@gmail.com>
2020-12-22 18:43:32 +0000

    code sample for #38601

---
+++ bisect/38601.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+print(pd.__version__)
+
+# create a dataframe with non-lexsorted multilevel columns
+df = pd.DataFrame(
+    np.arange(12).reshape(3, 4),
+    columns=pd.MultiIndex.from_tuples([("B", 1), ("B", 2), ("A", "3"), ("A", "4")]),
+)
+
+df.loc[:, "A"] = np.zeros(
+    (3, 2)
+)  # ValueError: Must have equal len keys and value when setting with an ndarray
+
+print(df)
