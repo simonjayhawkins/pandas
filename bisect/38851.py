e994627330114eda2d571085f83790aa1ccaa007
Simon Hawkins <simonjayhawkins@gmail.com>
2020-12-31 16:20:29 +0000

    code sample for #38851

---
+++ bisect/38851.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timezone
+
+import pandas as pd
+
+print(pd.__version__)
+
+
+df = pd.DataFrame(range(31))
+
+df["dt"] = pd.date_range("2020/12/01", "2020/12/31", tz="UTC")
+
+df["dt"].iloc[5] = pd.NaT
+
+df["dt"] = df["dt"].fillna(datetime(1980, 1, 1, tzinfo=timezone.utc))
+
+print(df)
