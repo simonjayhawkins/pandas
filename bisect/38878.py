62e6c1aa90207cbc61b0ed8c06a2c86c02bbda5f
Simon Hawkins <simonjayhawkins@gmail.com>
2021-01-01 13:35:49 +0000

    code sample for #38878

---
+++ bisect/38878.py
@@ -0,0 +1,14 @@
+import datetime as dt
+import pandas as pd
+
+print(pd.__version__)
+
+df = pd.DataFrame([[dt.date(2021, 1, 1), 1, "a"], [dt.date(2021, 1, 2), 2, "b"]])
+
+g = df.groupby([0, 1])
+print(g.groups.keys())
+
+result = next(iter(key[0] for key in g.groups.keys()))
+result
+
+assert isinstance(result, pd.Timestamp)
