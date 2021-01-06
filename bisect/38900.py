7912621cded240d965c1611bbd15d492d797ec9c
Simon Hawkins <simonjayhawkins@gmail.com>
2021-01-06 13:11:05 +0000

    code sample for #38900

---
+++ bisect/38900.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+print(pd.__version__)
+
+df = pd.DataFrame(["o", "o'doul's", "oad", "oaf", "oafish"])
+df = df.astype("|S")
+print(df)
+
+print(df.dtypes)
+
+df.replace({None: np.nan})
