91deb25a1d5c9a95dcc5de465272ed3039ebd9d0
Simon Hawkins <simonjayhawkins@gmail.com>
2021-01-26 14:15:35 +0000

    code sample for #39338

---
+++ bisect/39338.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+print(pd.__version__)
+
+data = df = pd.DataFrame(
+    {
+        "a_str": ["A1", "A2", "A3"],
+        "b_int": ["1,000", "200", "3"],
+        "c_str": ["C1", "C2", "C3"],
+        "d_date": ["2021-01-01", "", "2021-03-03"],
+    }
+)
+non_string_columns = ["b_int", "d_date"]
+
+df[non_string_columns] = df[non_string_columns].replace(regex={"": np.nan, ",": ""})
+
+print(df)
