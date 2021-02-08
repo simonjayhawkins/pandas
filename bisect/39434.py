bf29638a615a92b198faf7087e2b27c0f8012de8
Simon Hawkins <simonjayhawkins@gmail.com>
2021-02-08 16:17:21 +0000

    code sample for #39434

---
+++ bisect/39434.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+
+print(pd.__version__)
+
+df = pd.DataFrame(np.arange(64))
+length = len(df.index)
+df.index = [(i - length / 2) % length for i in range(length)]
+result = df.sort_index(axis=0, ascending=None, na_position="first")
+print(result)
