8352a6645d3bf6020d31d459183cccbf9ec03bb4
Simon Hawkins <simonjayhawkins@gmail.com>
2021-02-12 11:09:07 +0000

    update code sample #39738

--- bisect/39738.py
+++ bisect/39738.py
@@ -4,5 +4,9 @@ print(pd.__version__)
 
 df = pd.DataFrame({"x": [1, 2, 3], "y": [4, 5, 6]})
 
-result = df.sum(min_count=10)
-print(result)
+try:
+    result = df.sum(min_count=10)
+except AttributeError:
+    pass
+else:
+    print(result)
