f749f92e37035ec4eed17e1139d9527deb2d63fb
Simon Hawkins <simonjayhawkins@gmail.com>
2021-04-12 11:29:23 +0100

    code sample for #40371

---
+++ bisect/40371.py
@@ -0,0 +1,13 @@
+# BUG: Change in behavior of replace with integer series and float to_replace
+# #40371
+
+import numpy as np
+import pandas as pd
+
+print(pd.__version__)
+
+result = pd.Series([1]).replace(np.array([1.0]), [0])
+print(result)
+
+expected = pd.Series([0])
+pd.testing.assert_series_equal(result, expected)
