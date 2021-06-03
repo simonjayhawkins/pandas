081797e92d282cd686f2b973ee8f8606ab568c08
Simon Hawkins <simonjayhawkins@gmail.com>
2021-06-03 13:03:56 +0100

    add code sample for #41797

---
+++ bisect/41797.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+print(pd.__version__)
+
+df = pd.DataFrame(["a", "b", np.nan])
+
+expected = df.astype(str)
+
+result = df.astype("category").astype(str)
+print(result)
+
+pd.testing.assert_frame_equal(result, expected)
