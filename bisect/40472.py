bc12c51857cc9ce0ca1873d95b8eb34c4b5cd424
Simon Hawkins <simonjayhawkins@gmail.com>
2021-06-07 19:47:32 +0100

    add code sample for #40472

---
+++ bisect/40472.py
@@ -0,0 +1,13 @@
+# BUG: series.replace(np.nan,..) on categorical series does not replace #40472
+
+
+import numpy as np
+import pandas as pd
+
+print(pd.__version__)
+
+s = pd.Series({1: np.nan, 2: "b"}).astype("category")
+result = s.replace(np.nan, "c")
+print(result)
+
+assert result[1] == "c"
