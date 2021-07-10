a41ba3faed6533b4ee3eeae1768faaf60d0533f4
Simon Hawkins <simonjayhawkins@gmail.com>
2021-07-10 12:25:10 +0100

    code sample for #42396

---
+++ bisect/42396.py
@@ -0,0 +1,13 @@
+# BUG: astype() on an integer DataFrame changes the order of data #42396
+
+import numpy as np
+import pandas as pd
+
+print(pd.__version__)
+
+npa = np.random.RandomState(0).randint(1000, size=(20, 8))
+df = pd.DataFrame(npa, columns=[f"c{i}" for i in range(8)]).iloc[:6, :3]
+result = df.astype("int32")
+print(result)
+
+pd.testing.assert_frame_equal(result, df, check_dtype=False)
