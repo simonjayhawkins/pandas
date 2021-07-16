0071de31d025d23b8a3006e430739e2a9d432353
Simon Hawkins <simonjayhawkins@gmail.com>
2021-07-16 12:13:49 +0100

    code sample for #42456

---
+++ bisect/42456.py
@@ -0,0 +1,13 @@
+# BUG: Regression on DataFrame.from_records #42456
+
+from numpy import empty, array
+import pandas as pd
+print(pd.__version__)
+
+structured_dtype = [("prop", int)]
+
+# Does NOT work any more
+result = empty((0, len(structured_dtype)))
+structured_array = array(result, dtype=structured_dtype)
+result = pd.DataFrame.from_records(structured_array)
+print(result)
