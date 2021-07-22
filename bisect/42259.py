5460c3608e468afc6ed147f9a7392a1336e170ee
Simon Hawkins <simonjayhawkins@gmail.com>
2021-07-22 13:52:23 +0100

    code sample for #42259

---
+++ bisect/42259.py
@@ -0,0 +1,20 @@
+# BUG: With cache, to_datetime() returns pd.NaT for inputs that produce duplicated values #42259
+
+import pandas as pd
+
+print(pd.__version__)
+
+from pandas import NaT, to_datetime, Series, Timestamp
+from pandas._testing import assert_series_equal
+
+input_ser = Series(
+    [None] + [NaT] * 50 + ["2012 July 26", Timestamp("2012-07-26")], dtype="object"
+)
+expected_ser = Series(
+    [NaT] * 51 + [Timestamp("2012-07-26"), Timestamp("2012-07-26")],
+    dtype="datetime64[ns]",
+)
+result = to_datetime(input_ser)
+print(result)
+
+assert_series_equal(result, expected_ser)
