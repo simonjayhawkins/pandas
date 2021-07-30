31ce667328f4fde229b761b8fa6fdb2e7c9a44a2
Simon Hawkins <simonjayhawkins@gmail.com>
2021-07-30 12:37:05 +0100

    code sample for #42816

---
+++ bisect/42816.py
@@ -0,0 +1,11 @@
+# BUG: nlargest raises TypeError "No matching signature found" on Float64Dtype Series, versions >1.3.0 #42816
+
+import numpy as np
+import pandas as pd
+
+print(pd.__version__)
+
+s = pd.Series(np.random.random(10)).astype("Float64")
+
+result = s.nlargest(5)
+print(result)
