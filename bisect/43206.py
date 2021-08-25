ee93b9f3a8d14bbef6e273626d83b6d3a8eb08ba
Simon Hawkins <simonjayhawkins@gmail.com>
2021-08-25 13:10:51 +0100

    code sample for #43206

---
+++ bisect/43206.py
@@ -0,0 +1,33 @@
+# BUG: inconsistent groupby.apply behaviour depending on column dtypes #43206
+
+import pandas as pd
+
+print(pd.__version__)
+
+df = pd.DataFrame(
+    {
+        "date_val": {
+            0: pd.Timestamp("2017-01-01 00:00:00"),
+            1: pd.Timestamp("2017-01-01 00:00:00"),
+            2: pd.Timestamp("2017-02-01 00:00:00"),
+            3: pd.Timestamp("2017-02-01 00:00:00"),
+        },
+        "uid": {0: "1", 1: "2", 2: "1", 3: "2"},
+        "str_val": {
+            0: str("2017-01-01 00:00:00"),
+            1: str("2017-01-01 00:00:00"),
+            2: str("2017-02-01 00:00:00"),
+            3: str("2017-02-01 00:00:00"),
+        },
+    }
+)
+
+df1 = df.groupby("uid", as_index=False)[["uid", "str_val", "date_val"]].apply(
+    lambda x: x.sort_values(by="str_val", ascending=True)
+)
+print(df1)
+
+expected = pd.MultiIndex.from_tuples(
+    [(0, 0), (0, 2), (1, 1), (1, 3)],
+)
+pd.testing.assert_index_equal(df1.index, expected)
