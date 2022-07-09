f74e926f96d4cc188db4da3698d83bf2d33efc4f
Simon Hawkins <simonjayhawkins@gmail.com>
2022-07-09 19:20:27 +0100

    code sample for #47578

---
+++ bisect/47578.py
@@ -0,0 +1,15 @@
+# BUG: Setting a DataFrame as rhs with loc on a single column does not align in the single
+# block case #47578
+
+import numpy as np
+import pandas as pd
+
+print(pd.__version__)
+
+result = pd.DataFrame({"var": [1, 2]}, index=[1, 2])
+rhs = pd.DataFrame({"var": [10, 11]}, index=[2, 3])
+result.loc[:, "var"] = rhs
+print(result)
+
+expected = pd.DataFrame({"var": [np.nan, 10]}, index=[1, 2])
+pd.testing.assert_frame_equal(result, expected)
