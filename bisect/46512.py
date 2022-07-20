a9397b5d96fae221dc0515d1d24884bfeb6623d0
Simon Hawkins <simonjayhawkins@gmail.com>
2022-07-20 12:08:33 +0000

    code sample for #46512

---
+++ bisect/46512.py
@@ -0,0 +1,14 @@
+# BUG: Inconsistency in DataFrame.where between inplace and not inplace with na like value
+# for StringArray #46512
+
+import numpy as np
+import pandas as pd
+
+print(pd.__version__)
+
+df = pd.DataFrame({"A": ["1", "", "3"]}, dtype="string")
+df.where(df != "", np.nan, inplace=True)
+print(df)
+arr = df["A"]._values
+print(arr)
+assert arr[1] is pd.NA, type(arr[1])
