b3e935f5743a45edf08d435060379957265a4dbe
Simon Hawkins <simonjayhawkins@gmail.com>
2022-07-27 19:43:20 +0000

    update #47871

--- bisect/47871.py
+++ bisect/47871.py
@@ -1,9 +1,15 @@
 # BUG: to_csv requires escapechar unnecessarily when data contains
 # null byte \x00 (PowerShell only) #47871
 
+import csv
 import pandas as pd
 
 print(pd.__version__)
 
 df = pd.DataFrame({"A": ["\x00"]})
-df.to_csv("null_byte.csv", index=False)
+try:
+    df.to_csv("null_byte.csv", index=False)
+except csv.Error as e:
+    print(e)
+else:
+    exit(1) 
